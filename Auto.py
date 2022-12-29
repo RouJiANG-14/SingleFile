d406f29dae2d6ed4b57e9c46fafbd37266dfc1f2
RouJiANG <53442596+RouJiANG-14@users.noreply.github.com>
2022-12-29 11:16:14 +0800

    Create Auto.py

---
+++ Auto.py
@@ -0,0 +1,32 @@
+def pathwalk(path='.', result=[]):
+    dirlist = os.listdir(path=path)
+ 
+    for item in dirlist:
+        child = os.path.join(path, item)
+        if os.path.isfile(child):
+            result.append(child)
+            # print(child)
+        else:
+            pathwalk(child, result)
+def generate(files=[], outputpath='./readme.md'):
+    info = {}
+    for file in files:
+        if 'readme.' in str(file):
+            continue
+        with open(file, 'r', encoding='utf8') as f:
+            temp = getdescription(f.readlines())
+            temp = temp[15:]
+            info[str(file)] = temp
+            f.close()
+    # print(len(info), info)
+    info = {k:v for k,v in info.items() if v!='' and 'readme.' not in str(k)}
+    print(len(info), info)
+ 
+    # 生成Markdown文件
+    with open(outputpath, 'a', encoding='utf8') as f:
+        # f.write('标题部分\n---')
+        for key, value in info.items():
+            temp = " - [{}]({})\n\n".format(value, key)
+            f.write(temp)
+        f.close()
+    print('文件已生成！')
